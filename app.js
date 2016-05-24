ead2c8417d61bc852066e9fd6e7deabb837a0641
Kiefner <Kiefner3@gmx.de>
2016-05-24 21:56:16 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 
 var config = {
   // - Your app's id on moneypot.com
-  app_id: 18,                             // <----------------------------- EDIT ME!
+  app_id: 1170,                             // <----------------------------- EDIT ME!
   // - Displayed in the navbar
   app_name: 'K-DICE',
   // - For your faucet to work, you must register your site at Recaptcha
